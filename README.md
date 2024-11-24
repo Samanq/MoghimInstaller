8eade75854d20526ab13f935bc95832a2ce5233c
Saman Qaydi (Lap02) <saman.qaydi@gmail.com>
2024-11-24 14:48:59 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# MoghimInstaller
-A helper tool to install the moghim softwares
+# نصب نرم افزار های مقیم (هورگشت)
+
+۱. این [فایل](https://samanqaydi.com/MoghimInstaller.zip) را دانلود کنید و 
+سپس آن را از حالت فشرده خارج کنید.
+
+۲. سپس MoghimInstaller.exe را اجرا کنید و منتظر باشید تا فایل ها مورد نیاز دانلود و نصب شود.
+<div style="text-align: right;">
+
+![Moghim Installer1](assets/images/Moghim_Installer01.jpg)
+
+۳. زمانی که پیام نصب موفقیت آمیز را مشاهده کردید،‌میتوانید برنامه ها را روی دسکتاپ خود مشاهده کنید.
+
+![Moghim Installer2](assets/images/Moghim_Installer02.jpg)
+
+> نکته: برای دریافت کد فعال سازی نرم افزار با پشتیبانی تماس بگیرید.
+
+> اگر با مشکل مشاهده علامت سوال در برنامه مواجه شدید مراحل زیر را دنبال کیند
+
+۱. منو استارت را باز کرده و سپس برنامه Run را باز کنید.
+
+۲. عبارت intl.cpl را وارد کرده و OK را بزنید.
+
+![Moghim Installer3](assets/images/Moghim_Installer03.jpg)
+
+۳. سپس وارد سربرگ Administrative شوید و Change system locale را بزنید.
+
+۴. در آنجا از لیست گزینه Persian (Iran) را انتخاب کنید و OK را بزنید.
+
+۵. با درخواست Restart سیستم موافق کنید
+
+![Moghim Installer4](assets/images/Moghim_Installer04.jpg)
+
+
+</div>
---
+++ src/MoghimInstaller/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.IO.Compression;
+
+Console.WriteLine("Installer has been initiated!");
+
+string url = "https://samanqaydi.com/houragency.zip";
+string extractPath = @"C:\Moghim";
+
+await DownloadAndExtractZipFile(url, extractPath);
+
+static async Task DownloadAndExtractZipFile(string url, string extractPath)
+{
+    using (HttpClient client = new HttpClient())
+    {
+        using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+        {
+            response.EnsureSuccessStatusCode();
+
+            long? totalBytes = response.Content.Headers.ContentLength;
+            using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
+                          zipStream = new MemoryStream())
+            {
+                byte[] buffer = new byte[8192];
+                long totalRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    zipStream.Write(buffer, 0, bytesRead);
+                    totalRead += bytesRead;
+
+                    if (totalBytes.HasValue)
+                    {
+                        Console.SetCursorPosition(0, 1);
+                        Console.WriteLine($"Download progress: {totalRead * 100 / totalBytes.Value}%");
+                    }
+                }
+
+                zipStream.Seek(0, SeekOrigin.Begin);
+                using (ZipArchive archive = new ZipArchive(zipStream))
+                {
+                    archive.ExtractToDirectory(extractPath, true);
+                }
+            }
+        }
+    }
+
+    string sourceFilePath = Path.Combine(extractPath, "Agency\\Agency.lnk");
+    string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+    string destinationFilePath = Path.Combine(desktopPath, "Agency.lnk");
+
+    if (File.Exists(sourceFilePath))
+    {
+        File.Copy(sourceFilePath, destinationFilePath, true);
+        Console.WriteLine("Shortcut copied to desktop successfully.");
+    }
+    else
+    {
+        Console.WriteLine("Shortcut file not found.");
+    }
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Moghim Installed successfully");
+    Console.ResetColor();
+    Console.WriteLine("Press any key to quit");
+    Console.ReadKey();
+}
